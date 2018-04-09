c9501cf80e34fc860f6d77e84ee3ac64f7d53c34
Alvaro Monferrer <AlvaroMonferrer@192.168.0.18>
2018-04-09 02:08:21 +0200

    Creacion Empresa

--- src/main/java/es/uji/ei1027/btas/model/Empresa.java
+++ src/main/java/es/uji/ei1027/btas/model/Empresa.java
@@ -1,33 +1,27 @@
 package es.uji.ei1027.btas.model;
 
-import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
 
-public class Empresa {
+public class Empresa extends Contacto{
 
-	private String cif;
-	private Direccion direccion;
-	private String nombre;
-	private String telefono;
-	private String email; 
 	private String direccionWeb;
-	private Set<Estancia> ofertasEstancia;
+	private HashSet<Estancia> ofertasEstancia;
+	private List<PersonaContacto> personasContacto;
 
 
 	public Empresa() {
 		super();
 	}
 
-	public Empresa(String cif, Direccion direccion, String nombre, String telefono, String email, String direccionWeb,Set<Estancia> ofertasEstancia ) {
-		this.cif = cif;
-		this.direccion = direccion;
-		this.nombre = nombre;
-		this.telefono =telefono;
-		this.email = email;
+	public Empresa(int cif, List<Direccion> direccion, String nombre, int telefono, String email, String direccionWeb, HashSet<Estancia> ofertasEstancia, List<PersonaContacto> personas ) {
+		super(cif,direccion,nombre,telefono,email);
 		this.direccionWeb = direccionWeb;
 		this.ofertasEstancia = ofertasEstancia;
+		this.personasContacto=personas;
 
 	}
-	public Empresa(Empresa empresa ) {
+	/*public Empresa(Empresa empresa ) {
 		this.cif = empresa.cif;
 		this.direccion = empresa.direccion;
 		this.nombre = empresa.nombre;
@@ -36,6 +30,31 @@ public class Empresa {
 		this.direccionWeb= empresa.direccionWeb;
 		this.ofertasEstancia = empresa.ofertasEstancia;
 
+	}*/
+
+	public void setDireccionWeb(String web) {
+		direccionWeb=web;
 	}
+	
+	public String getDireccionWeb() {
+		return direccionWeb;
+	}
+	
+	public HashSet<Estancia> getOfertasEstancia(){
+		return ofertasEstancia;
+	}
+	
+	public void setOfertasEstancia(HashSet<Estancia> nuevasEstancias) {
+		ofertasEstancia=nuevasEstancias;
+	}
+	
+	public void setPersonasContacto(List<PersonaContacto> personas) {
+		personasContacto=personas;
+	}
+	
+	public List<PersonaContacto> getPersonasContacto(){
+		return personasContacto;
+	}
+
 	
 }
