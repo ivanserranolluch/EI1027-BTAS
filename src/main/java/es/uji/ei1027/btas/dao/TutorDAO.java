1207f1ae86b5438b34b462984e6da5afb93a7be0
DoomedAl <al315554@uji.es>
2018-05-09 18:28:36 +0200

    Creacion TutorDAO

---
+++ src/main/java/es/uji/ei1027/btas/dao/TutorDAO.java
@@ -0,0 +1,40 @@
+package es.uji.ei1027.btas.dao;
+
+import java.util.List;
+
+import es.uji.ei1027.btas.model.Tutor;
+
+public class TutorDAO implements TutorDAOInterface{
+
+	@Override
+	public List<Tutor> getTutores() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Tutor getTutor(String dni) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addTutor(Tutor tutor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateTutor(String dni, int id_dir, String nombre, int telefono, String email, String departamento,
+			String despacho) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteTutor(String dni) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
