bac5cc1dc36583a9b7138997548332a1c8583667
DoomedAl <al315554@uji.es>
2018-05-09 18:27:46 +0200

    Actualización tutor

--- src/main/java/es/uji/ei1027/btas/model/Tutor.java
+++ src/main/java/es/uji/ei1027/btas/model/Tutor.java
@@ -10,17 +10,72 @@ public class Tutor extends Contacto {
 	
 	public Tutor(){super();}
 	
-	public Tutor(String dni, Direccion direccion, String nombre,String apellidos, String telefono, String email,String departamento,String despacho  ) {
-	super();
-	this.apellidos = apellidos;
+	public Tutor(String dni, int id_dir, String nombre, int telefono, String email,String departamento,String despacho  ) {
+	super(dni, id_dir,nombre,telefono,email);
 	this.departamento= departamento;
 	this.despacho=despacho;
 	}
 	public Tutor(Tutor tutor ) {
 	super();
-	this.apellidos = tutor.apellidos;
 	this.departamento= tutor.departamento;
 	this.despacho= tutor.despacho;
 	}
 
+	public String getDepartamento() {
+		return departamento;
+	}
+
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+
+	public String getDespacho() {
+		return despacho;
+	}
+
+	public void setDespacho(String despacho) {
+		this.despacho = despacho;
+	}
+	
+	public String getDni(){
+		return super.getId();
+	}
+	
+	public void setDni(String nuevoDni) {
+		super.setId(nuevoDni);
+	}
+	
+	public int getDireccion(){
+		return super.getDireccion();
+	}
+	
+	public String getNombre() {
+		return super.getNombre();
+	}
+	
+	public int getTelefono() {
+		return super.getTelefono();
+	}
+	
+	public String getEmail() {
+		return super.getEmail();
+	}
+	
+	public void setDirecciones(int idDirNuevo) {
+		super.setDirecciones(idDirNuevo);
+	}
+	
+	public void setNombre(String nombreNuevo) {
+		super.setNombre(nombreNuevo);
+	}
+	
+	public void setTelefono(int telefonoNuevo) {
+		super.setTelefono(telefonoNuevo);
+	}
+	
+	public void setEmail(String emailNuevo) {
+		super.setEmail(emailNuevo);
+	}
+	
 }
+
