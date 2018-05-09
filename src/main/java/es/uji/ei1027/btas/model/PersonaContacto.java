dee58a24cbd59afd72d8624427ff716f4fb5af54
DoomedAl <al315554@uji.es>
2018-05-09 18:01:53 +0200

    Eliminacion de dependencias

--- src/main/java/es/uji/ei1027/btas/model/PersonaContacto.java
+++ src/main/java/es/uji/ei1027/btas/model/PersonaContacto.java
@@ -1,6 +1,6 @@
 package es.uji.ei1027.btas.model;
 
-public class PersonaContacto extends Persona{
+public class PersonaContacto{
 
 	private String apellidos;
 	private Empresa empresa;
@@ -12,13 +12,11 @@ public class PersonaContacto extends Persona{
 	}
 
 	public PersonaContacto(String dni, Direccion direccion, String nombre,String apellidos, String telefono, String email, Empresa empresa, Estancia estancia ) {
-		super( dni,  direccion,  nombre,  telefono, email );
 		this.apellidos=apellidos;
 		this.empresa= empresa;
 		this.estancia=estancia;
 	}
 	public PersonaContacto(PersonaContacto personaContacto) {
-		super(personaContacto);
 		this.apellidos=personaContacto.apellidos;
 		this.empresa= personaContacto.empresa;
 		this.estancia=personaContacto.estancia;
--- src/main/java/es/uji/ei1027/btas/model/Tutor.java
+++ src/main/java/es/uji/ei1027/btas/model/Tutor.java
@@ -1,6 +1,9 @@
 package es.uji.ei1027.btas.model;
 
-public class Tutor extends Persona {
+
+
+
+public class Tutor extends Contacto {
 	
 	private String apellidos;
 	private String departamento;
@@ -9,13 +12,13 @@ public class Tutor extends Persona {
 	public Tutor(){super();}
 	
 	public Tutor(String dni, Direccion direccion, String nombre,String apellidos, String telefono, String email,String departamento,String despacho  ) {
-	super( dni,  direccion,  nombre,  telefono, email );
+	super();
 	this.apellidos = apellidos;
 	this.departamento= departamento;
 	this.despacho=despacho;
 	}
 	public Tutor(Tutor tutor ) {
-	super( tutor );
+	super();
 	this.apellidos = tutor.apellidos;
 	this.departamento= tutor.departamento;
 	this.despacho= tutor.despacho;
