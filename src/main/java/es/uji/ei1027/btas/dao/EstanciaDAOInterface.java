cd161ee0a7f3d25df3bdc4289b46b69e6b9721ad
Jessica <Jessica@10.129.31.22>
2018-05-15 18:58:12 +0200

    arreglar master

--- src/main/java/es/uji/ei1027/btas/dao/EstanciaDAOInterface.java
+++ src/main/java/es/uji/ei1027/btas/dao/EstanciaDAOInterface.java
@@ -1,5 +1,25 @@
 package es.uji.ei1027.btas.dao;
 
+import java.util.Date;
+import java.util.List;
+
+import es.uji.ei1027.btas.model.Estancia;
+import es.uji.ei1027.btas.model.Itinerario;
+
+
 public interface EstanciaDAOInterface {
 
+	public List<Estancia> getEstancias();
+	
+	
+	public void addEstancia(Estancia estancia);
+	
+	public void updateOfertaProyectoEstado(int id,String estado);
+	
+	public void updateOfertaProyecto(int id, String estado, String itinerario, int id_estancia, Date fechaAlta, String objetvio, String tarea);
+	
+	public void deleteEstancia(int id);
+	
+	
+	
 }
